1067eb379f9a2752f2e29eb24c1a53142ad0e511
ghorsey <geoff.horsey@outlook.com>
2023-01-16 22:32:36 -0800

    feat: added <C-P> to bring up fuzzy files

--- lua/keys.lua
+++ lua/keys.lua
@@ -35,6 +35,7 @@ map('n', "<leader>nt", ":NvimTreeToggle<CR>")
 -- Telescope shortcuts
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
