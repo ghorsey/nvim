6a7cfdb04aa8c3031580109fe41dd636b18420b6
ghorsey <geoff.horsey@outlook.com>
2023-01-18 09:43:33 -0800

    feat: updated gitignore and removed unnecessary lsp config file

--- lua/gah/set.lua
+++ lua/gah/set.lua
@@ -54,10 +54,10 @@ vim.g.nofoldenable = true
 -- shortmess: avoid shoing extra messages when using completion
 -- updatedtime: set updatetime for CursorHold
 
--- vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert'}
--- vim.opt.shortmess = vim.opt.shortmess + { c = true }
--- vim.api.nvim_set_option('updatetime', 300)
---
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
+vim.api.nvim_set_option('updatetime', 300)
+
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on tcursor hover_range
 -- Goto previous / next diagnostic warning / error
