8a84799170be15da3fe74ff089f60eb85500d535
ghorsey <geoff.horsey@outlook.com>
2023-01-17 19:59:05 -0800

    feat: moved function into utils module

--- init.lua
+++ init.lua
@@ -1,19 +1,7 @@
--- Ensure packer is installed when first cloning config files
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
-end
-
 -- local packer_bootstrap = ensure_packer() -- original line
-ensure_packer()
+require("gah.utils").ensure_packer()
 
--- setup impatient
+-- speed up loading
 require("impatient")
 
 require("gah")
