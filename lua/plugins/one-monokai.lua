4b4fc196f016b2beab24282d5d483b3ee311f4b4
ghorsey <geoff.horsey@outlook.com>
2023-06-13 00:42:18 -0700

    feat: disabled italics and fixed error in typescript

--- lua/plugins/one-monokai.lua
+++ lua/plugins/one-monokai.lua
@@ -1,9 +1,10 @@
 return {
   {
-    'cpea2506/one_monokai.nvim',
+    "cpea2506/one_monokai.nvim",
     config = function()
-      require('one_monokai').setup({
+      require("one_monokai").setup({
+        italics = false,
       })
-    end
-  }
+    end,
+  },
 }
