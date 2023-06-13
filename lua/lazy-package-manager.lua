dfc5e9a33f4c2b4ca02d9afc21a6dd4256c71e19
Geoff Horsey <geoff.horsey@jumpgatesolutions.com>
2023-06-12 17:40:20 -0700

    feat: added copilot

--- lua/lazy-package-manager.lua
+++ lua/lazy-package-manager.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    import = "plugins",
+    { import = "plugins" },
+    { import = "plugins.extras.coding.copilot" },
   },
 })
