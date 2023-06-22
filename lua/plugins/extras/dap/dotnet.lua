5aee078f83db37f3929205f86e8ba759392a1f09
Geoff Horsey <geoff.horsey@jumpgatesolutions.com>
2023-06-21 17:40:16 -0700

    feat: added dotnet as a language

---
+++ lua/plugins/extras/dap/dotnet.lua
@@ -0,0 +1,76 @@
+local function cmd(adapter)
+  local c = vim.fn.stdpath("data") .. "/mason/bin/" .. adapter
+
+  if jit.os == "Windows" then
+    return c .. ".cmd"
+  end
+
+  return c
+end
+
+-- Helpers for dotnet
+vim.g.dotnet_build_project = function()
+  local default_path = vim.fn.getcwd() .. "/"
+  if vim.g["dotnet_last_proj_path"] ~= nil then
+    default_path = vim.g["dotnet_last_proj_path"]
+  end
+  local path = vim.fn.input("Path to your *proj file", default_path, "file")
+  vim.g["dotnet_last_proj_path"] = path
+  local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+  print("")
+  print("Cmd to execute: " .. cmd)
+  local f = os.execute(cmd)
+  if f == 0 then
+    print("\nBuild: ✔️ ")
+  else
+    print("\nBuild: ❌ (code: " .. f .. ")")
+  end
+end
+
+vim.g.dotnet_get_dll_path = function()
+  local request = function()
+    return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+  end
+
+  if vim.g["dotnet_last_dll_path"] == nil then
+    vim.g["dotnet_last_dll_path"] = request()
+  else
+    if
+      vim.fn.confirm("Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2) == 1
+    then
+      vim.g["dotnet_last_dll_path"] = request()
+    end
+  end
+
+  return vim.g["dotnet_last_dll_path"]
+end
+
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    print("i'm in my config")
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = cmd("netcoredbg"),
+      args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+          if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+            vim.g.dotnet_build_project()
+          end
+          return vim.g.dotnet_get_dll_path()
+        end,
+      },
+    }
+    print(vim.inspect(dap.adapters))
+    print(vim.inspect(dap.configurations))
+  end,
+}
