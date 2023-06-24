adb38e592eadf79d36777b8400b40be25a91b094
ghorsey <geoff.horsey@outlook.com>
2023-06-24 14:00:27 -0700

    feat: fix issue with luasnip jumping back to previously replacement mark

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -80,3 +80,18 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
+
+-- Fix issue with luasnip keeping the session mode open
+-- https://github.com/L3MON4D3/LuaSnip/issues/258
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
+})
